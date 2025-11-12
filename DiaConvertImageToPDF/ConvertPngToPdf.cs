e2a37cf6c59725fe426b368cd3c29757ed512ede
Jonathan Colon <rebelinux@gmail.com>
2025-11-11 22:26:27 -0400

    Add project.nuget.cache file with package dependencies for DiaConvertImageToPDF

---
+++ DiaConvertImageToPDF/ConvertPngToPdf.cs
@@ -0,0 +1,50 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.IO.Image;
+using System.IO;
+using System;
+
+
+namespace DiaConvertImageToPDF
+{
+    public class ConvertImageToPDF
+    {
+        public static bool ConvertPngToPdf(string pngFilePath, string pdfFilePath)
+        {
+            try
+            {
+                // Create a new PDF document
+                using var pdfWriter = new PdfWriter(pdfFilePath);
+                using var pdfDocument = new PdfDocument(pdfWriter);
+
+                // Get image dimensions to set page size
+                var tempImageData = ImageDataFactory.Create(pngFilePath);
+                var pageSize = new iText.Kernel.Geom.PageSize(tempImageData.GetWidth(), tempImageData.GetHeight());
+                pdfDocument.SetDefaultPageSize(pageSize);
+
+                // Create Document with page properties
+                var document = new Document(pdfDocument);
+
+                // Load the PNG image
+                ImageData imageData = ImageDataFactory.Create(pngFilePath);
+                Image image = new Image(imageData);
+
+                // Adjust image size
+                image.SetAutoScaleWidth(true); // Scale image to fit page width
+                image.SetAutoScaleHeight(true);
+                // Add the image to the document
+                document.Add(image);
+
+                // Close the document
+                document.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error generating pdf from image: {ex.Message}");
+                return false; // Or throw a more specific exception
+            }
+        }
+    }
+}
---
+++ DiaConvertImageToPDF/DiaConverterImageToPDF.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DiaConvertImageToPDF
+{
+    public class MainEntry
+    {
+        public static void Main(string[] args)
+        {
+            // var convert = new ConvertImageToPDF();
+            // convert.ConvertPngToPdf("/home/rebelinux/foto.png", "/home/rebelinux/foto.pdf");
+        }
+
+    }
+}
